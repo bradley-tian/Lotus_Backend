8685ecb2eb1cf3db5edd051274abb4180a429f54
Bradley Tian <bradley_tian@berkeley.edu>
2022-07-23 01:23:58 -0700

    changed init file

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-"""Runs the app on (currently the test server)"""
-from app import app
+"""Runs the app."""
+from app import create_app
+
+app = create_app() 
 
 if __name__ == "__main__":
     app.run()
